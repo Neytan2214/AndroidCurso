a3a50fb5607eec3f099212b671e0660ff66e5b68
Neytan2214 <ney.silva@duocu.cl>
2025-08-27 11:26:15 -0400

    se agrego spinner al mainact4

--- app/src/main/java/com/example/proyectobase2/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase2/MainActivity4.kt
@@ -3,6 +3,7 @@ package com.example.proyectobase2
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +20,8 @@ class MainActivity4 : AppCompatActivity() {
         val edNumero2:EditText=findViewById(R.id.edNumero2)
         val btnCalcular:Button=findViewById(R.id.btnCalcular)
         val txResultado:TextView=findViewById(R.id.txResultado)
+        val spOperaciones:Spinner=findViewById(R.id.sp_operaciones)
+        val menuSPoperaciones= listOf("Sumar","Restar","Multiplicar","Dividir")
         btnCalcular.setOnClickListener {
             var calcN1:Int=edNumero1.text.toString().toIntOrNull()?:0
             var calcN2:Int=edNumero2.text.toString().toIntOrNull()?:0
