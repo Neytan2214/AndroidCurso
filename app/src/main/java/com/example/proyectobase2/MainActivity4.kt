d4113dded98beb76dbf74daa96a0776a632e3b92
Neytan2214 <ney.silva@duocu.cl>
2025-08-27 10:36:11 -0400

    se hicieron val en el Mainactiviti4

--- app/src/main/java/com/example/proyectobase2/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase2/MainActivity4.kt
@@ -1,6 +1,8 @@
 package com.example.proyectobase2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class MainActivity4 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main4)
+        val edNumero1:EditText=findViewById(R.id.edNumero1)
+        val edNumero2:EditText=findViewById(R.id.edNumero2)
+        val btnCalcular:Button=findViewById(R.id.btnCalcular)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
