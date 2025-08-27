f18b743555570fa509848959de6321901292c3f8
Neytan2214 <ney.silva@duocu.cl>
2025-08-27 10:50:43 -0400

    se agrego la clase opMatematicas

--- app/src/main/java/com/example/proyectobase2/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase2/MainActivity4.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectobase2.utlis.OpMatematicas;
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/proyectobase2/utlis/OpMatematicas.kt
@@ -0,0 +1,7 @@
+package com.example.proyectobase2.utlis
+
+object OpMatematicas {
+    fun sumar(n1:Int,n2:Int):Int{
+        return n1+n2
+    }
+}
