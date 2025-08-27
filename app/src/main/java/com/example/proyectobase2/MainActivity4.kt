f195860a8de3ffbd4a77746c21f168113b75f631
Neytan2214 <ney.silva@duocu.cl>
2025-08-27 11:36:59 -0400

    se mejoro

--- app/src/main/java/com/example/proyectobase2/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase2/MainActivity4.kt
@@ -1,6 +1,7 @@
 package com.example.proyectobase2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
@@ -22,6 +23,9 @@ class MainActivity4 : AppCompatActivity() {
         val txResultado:TextView=findViewById(R.id.txResultado)
         val spOperaciones:Spinner=findViewById(R.id.sp_operaciones)
         val menuSPoperaciones= listOf("Sumar","Restar","Multiplicar","Dividir")
+        val adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,menuSPoperaciones)
+        spOperaciones.adapter=adapter
+
         btnCalcular.setOnClickListener {
             var calcN1:Int=edNumero1.text.toString().toIntOrNull()?:0
             var calcN2:Int=edNumero2.text.toString().toIntOrNull()?:0
