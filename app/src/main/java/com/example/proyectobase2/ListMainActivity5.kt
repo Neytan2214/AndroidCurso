78f3a4ebcf53cf86bd2d3f9aa0145e613d73483d
Neytan2214 <ney.silva@duocuc.cl>
2025-09-01 11:56:43 -0400

    se agrego list activity

---
+++ app/src/main/java/com/example/proyectobase2/ListMainActivity5.kt
@@ -0,0 +1,24 @@
+package com.example.proyectobase2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListMainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_main5)
+        val usuarioDesdeOtroActivity=intent.getStringExtra("pUsuario")
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
