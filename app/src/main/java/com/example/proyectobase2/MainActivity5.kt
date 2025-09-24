a41412831897d44e440bddf436697b36d5af3fcb
Neytan2214 <ney.silva@duocuc.cl>
2025-09-24 10:44:48 -0300

    ---se agregan dependencias y permisos para utilizar apis---

---
+++ app/src/main/java/com/example/proyectobase2/MainActivity5.kt
@@ -0,0 +1,25 @@
+package com.example.proyectobase2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main5)
+        val spRegion:Spinner=findViewById(R.id.sp_regiones)
+        val spComuna:Spinner=findViewById(R.id.sp_comunas)
+        val btnOpcion:Button=findViewById(R.id.btn_opcion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
